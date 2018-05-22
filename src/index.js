ddbf931e9352c463d1162212ad0e8c5c12497ffa
LucsT <lucas@cozycloud.cc>
2018-05-22 16:07:29 +0200

    1st working version, extract pdf from html

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ const {
   saveBills,
   log
 } = require('cozy-konnector-libs')
-const request = requestFactory({ cheerio: true, debug: true, jar: true })
+const request = requestFactory({
+  cheerio: true,
+  //  debug: true,
+  jar: true
+})
+const html2pdf = require('./html2pdf')
+const pdf = require('pdfjs')
+const moment = require('moment')
+moment.locale('fr')
 
 const baseUrl = 'https://auchandrive.fr/drive'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.login, fields.password)
@@ -23,40 +28,38 @@ async function start(fields) {
   const $ = await request(`${baseUrl}/client/mescommandes`)
   log('info', 'Parsing list of documents')
   const bills = await parseDocuments($)
-  log('debug', `Bills are :\n${bills}`)
-  return
-//  bills = [{filename: 'toto.pdf', fileurl: 'https://auchandrive.fr/drive/impression/imprimeanciennecommande/57406933'}]
-
-
+  log('info', `${bills.length} bills has been found`)
   log('info', 'Saving data to Cozy')
   await saveBills(bills, fields.folderPath, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
+    identifiers: ['bills']
   })
 }
 
 async function authenticate(username, password) {
   // Fetch a spefic shop to get the auchanCook="985|" cookie and avoid a systematic redirect
   // 302 to prehome
-  await request({url: `${baseUrl}/mag/St-Quentin-985/`,
-                 headers : {
-                   'Referer': `${baseUrl}/prehome/`
-                 }
-                })
+  await request({
+    url: `${baseUrl}/mag/St-Quentin-985/`,
+    headers: {
+      Referer: `${baseUrl}/prehome/`
+    }
+  })
   await signin({
     url: `${baseUrl}/client/identification`,
     formSelector: 'form[name="formIdentification"]',
-    formData: { emailValidate: username,
-                passwordValidate: password,
-                't-zoneid': 'identification'
-              },
+    formData: {
+      emailValidate: username,
+      passwordValidate: password,
+      't-zoneid': 'identification'
+    },
     validate: (statusCode, $) => {
       if ($('span[title="Me connecter"]').length === 0) {
         return true
       } else {
-        log('error', 'Span[title="Me connecter"] found in page, login have failed')
+        log(
+          'error',
+          'Span[title="Me connecter"] found in page, login have failed'
+        )
         return false
       }
     }
@@ -71,23 +74,58 @@ async function parseDocuments($) {
       shop: 'td:nth-child(2)',
       date: {
         sel: 'td:nth-child(3)',
-        parse: date => date.split(' ')[0]
+        parse: date => moment(date.split(' ')[0], 'L')
       },
       amount: {
         sel: 'td:nth-child(5)',
-        parse: amount => amount.split(' ')[0].replace(',','.')
+        parse: amount => parseFloat(amount.split(' ')[0].replace(',', '.'))
+      },
+      rawAmount: {
+        sel: 'td:nth-child(5)',
+        parse: amount => amount.replace(' ', '')
       },
       status: 'td:nth-child(7)'
     },
     'table tr:not(:nth-child(1))'
   )
-  // Needed to remove canceled 'Annulée' bills
+  // Need to remove canceled 'Annulée' bills
   bills = bills.filter(bill => bill.status == 'Retirée')
-  return bills.map(bill => ({
-    ...bill,
-    filename: `${bill.date}_${bill.amount.replace('.',',')}€_${bill.orderNumber}.pdf}`,
-    fileUrl: `${baseUrl}/impression/imprimeanciennecommande/${bill.orderNumber}`,
-    currency: '€',
-    vendor: 'Auchandrive'
-  }))
+  // Add html as stream manually for each
+  for (let i = 0, len = bills.length; i < len; i++) {
+    bills[i].filestream = await billURLToStream(
+      `${baseUrl}/impression/imprimeanciennecommande/${bills[i].orderNumber}`
+    )
+  }
+  // Finalize bill objects
+  bills = bills.map(function(bill) {
+    ;(bill.filename = `${bill.date.format('YYYY-MM-DD')}_${bill.rawAmount}_${
+      bill.orderNumber
+    }.pdf`),
+      (bill.date = bill.date.toDate())
+    delete bill.status
+    delete bill.rawAmount
+    ;(bill.currency = '€'), (bill.vendor = 'auchandrive')
+    return bill
+  })
+  return bills
+}
+
+async function billURLToStream(url) {
+  var doc = new pdf.Document()
+  const cell = doc.cell({ paddingBottom: 0.5 * pdf.cm }).text()
+  cell.add(
+    'Généré automatiquement par le connecteur Cozy AuchanDrive depuis la page ',
+    {
+      font: require('pdfjs/font/Helvetica-Bold'),
+      fontSize: 14
+    }
+  )
+  cell.add(url, {
+    link: url,
+    color: '0x0000FF'
+  })
+  const $ = await request(url)
+  html2pdf($, doc, $('body'), { baseURL: url })
+  doc.end()
+  return doc
 }
